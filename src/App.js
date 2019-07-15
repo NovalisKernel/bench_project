bd0ada5226576c84df4f0cd361f3afc9a5e95a52
Vadzim Sheukunou <Vadzim.Sheukunou@itechart-group.com>
2019-07-15 14:51:13 +0300

    Initial commit, add dependencies and routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import LoginComponent from "./components/Login/LoginComponent";
+import Header from "./components/common/Header";
+import EmployeesList from "./components/Employees/EmployeesList";
+import NewEmployeeComponent from "./components/NewEmployee/NewEmployeeComponent";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={EmployeesList} />
+          <Route path="/login" component={LoginComponent} />
+          <Route path="/new-employee" component={NewEmployeeComponent} />
+        </Switch>
+      </Router>
+    </Provider>
   );
 }
 

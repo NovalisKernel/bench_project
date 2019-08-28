903f41072372cf165143e189667f546178610574
Vadzim Sheukunou <Vadzim.Sheukunou@itechart-group.com>
2019-08-28 16:16:50 +0300

    Add new header

---
+++ src/components/common/HeaderWithTopDrawer.js
@@ -0,0 +1,176 @@
+import React from "react";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import Filters from "../Employees/Filters";
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex"
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    })
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    })
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  hide: {
+    display: "none"
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  a: {
+    textDecoration: "none",
+    color: "inherit"
+  },
+  title: {
+    flexGrow: 1
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: "0 8px",
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end"
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen
+    }),
+    marginLeft: -drawerWidth
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen
+    }),
+    marginLeft: 0
+  },
+  secondToolBar: {
+    borderTop: "1px solid rgb(235, 235, 235)",
+    borderBottom: "1px solid rgb(235, 235, 235)",
+    backgroundColor: "white"
+  }
+}));
+
+export default function HeaderWithTop(props) {
+  const {
+    isAuthenticate,
+    user,
+    values,
+    handleChange,
+    handleSkillChange,
+    handleFilter,
+    handleClear,
+    skills,
+    open,
+    handleDrawerClose,
+    handleDrawerOpen
+  } = props;
+  function handlerLogout(e) {
+    e.preventDefault();
+    props.logout();
+  }
+  const classes = useStyles();
+  const theme = useTheme();
+  //   const [open, setOpen] = React.useState(false);
+
+  //   function handleDrawerOpen() {
+  //     setOpen(true);
+  //   }
+
+  //   function handleDrawerClose() {
+  //     setOpen(false);
+  //   }
+
+  const drawer = (
+    <div>
+      <div className={classes.drawerHeader}>
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === "ltr" ? (
+            <ChevronLeftIcon />
+          ) : (
+            <ChevronRightIcon />
+          )}
+        </IconButton>
+      </div>
+      <Divider />
+      <Filters
+        values={values}
+        handleChange={handleChange}
+        handleSkillChange={handleSkillChange}
+        handleFilter={handleFilter}
+        handleClear={handleClear}
+        skills={skills}
+      />
+    </div>
+  );
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            <Link className={classes.a} to="/">
+              iTechArt bench info
+            </Link>
+          </Typography>
+          {user && user.position === "Group Manager" ? (
+            <Typography variant="h6">
+              <Link className={classes.a} to="/new-employee">
+                <Button color="inherit">Add new</Button>
+              </Link>
+            </Typography>
+          ) : null}
+          {isAuthenticate ? (
+            <Button color="inherit" onClick={handlerLogout}>
+              Logout
+            </Button>
+          ) : (
+            <Typography variant="h6">
+              <Link className={classes.a} to="/login">
+                <Button color="inherit">Login</Button>
+              </Link>
+            </Typography>
+          )}
+        </Toolbar>
+        <Toolbar className={classes.secondToolBar}>
+          <Typography variant="h6">HELLO</Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
